c142ae2f11821d4c6c71bc6978b3cd8e8317772a
Dmitry Kuznetsov <kuznetsov2d@gmail.com>
2013-05-07 19:07:57 +0400

    Update main.js

--- main.js
+++ main.js
@@ -3,44 +3,45 @@
  * Available via the MIT or new BSD license.
  * @see https://github.com/dmkuznetsov/requirejs-registry
  */
+define('registry',
+    [],
+    function () {
+        "use strict";
+        var repository = {};
 
-define( 'registry', [], function() {
-    "use strict";
-    var repository = {};
+        return {
+            load : function (name, req, load, config) {
+                var namespace = name.replace(/\//g, '_');
+                name = name.replace(/\/+$/, '');
 
-    return {
-        load: function ( name, req, load, config ) {
-            var namespace = name.replace( /\//g, '_' );
-            name = name.replace( /\/+$/, '' );
-
-            if ( repository[ namespace ] == undefined ) {
-                repository[ namespace ] = {};
-                if ( config.config != undefined && config.config[ name ] != undefined ) {
-                    repository[ namespace ] = config.config[ name ];
-                }
-            }
-
-            var instance = {
-                'version': '2.1.5'
-                , 'namespace': namespace
-                , 'all': function() {
-                    return repository[ this.namespace ];
-                }
-                , 'get': function( name ) {
-                    return repository[ this.namespace ][ name ];
-                }
-                , 'set': function( name, value ) {
-                    if ( name instanceof Object && value === undefined ) {
-                        for( var i in name ) {
-                            repository[ this.namespace ][ i ] = name[ i ];
-                        }
-                        return true;
+                if (repository[namespace] === undefined) {
+                    repository[namespace] = {};
+                    if (config.config !== undefined && config.config[name] !== undefined) {
+                        repository[namespace] = config.config[name];
                     }
-                    console.log( this.namespace, name, value );
-                    return repository[ this.namespace ][ name ] = value;
                 }
-            };
-            load( instance );
-        }
-    };
-});
+
+                var instance = {
+                    'version' : '2.1.6',
+                    'namespace' : namespace,
+                    'all' : function () {
+                        return repository[this.namespace];
+                    },
+                    'get' : function (name) {
+                        return repository[this.namespace][name];
+                    },
+                    'set' : function (name, value) {
+                        if (name instanceof Object && value === undefined) {
+                            for (var i in name) {
+                                repository[this.namespace][i] = name[i];
+                            }
+                            return true;
+                        }
+                        repository[this.namespace][name] = value;
+                        return repository[this.namespace][name];
+                    }
+                };
+                load(instance);
+            }
+        };
+    });
